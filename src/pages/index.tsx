d8637793fb01f3b6f1c91a908bbf562cf6d98144
KRISTIJAN KOCEV <kristijankocev1234@gmail.com>
2023-06-03 20:39:23 +0200

    Posting loading states plus react toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import type { RouterOutputs } from "~/utils/api";
 
 import dayjs from 'dayjs'
 import relativeTime from "dayjs/plugin/relativeTime"
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -17,7 +18,7 @@ const CreatePostWizard = () => {
 
 
   const { user } = useUser();
- 
+
   const [input, setInput] = useState("");
 
   const ctx = api.useContext();
@@ -26,6 +27,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to Post! Please try again later.")
+      }
     }
   });
 
@@ -38,15 +47,32 @@ const CreatePostWizard = () => {
       alt="Profile Image"
       width={56}
       height={56}
-       />
+    />
     <input placeholder="Type Some emojis"
       className="bg-transparent grow outline-none"
-      type="text" 
+      type="text"
       value={input}
       onChange={(e) => setInput(e.target.value)}
       disabled={isPosting}
-      />
-      <button onClick={() => mutate({content : input})}>Post</button>
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          if (input !== "") {
+            mutate({ content: input});
+          }
+        }
+      }}
+    />
+    {input !== "" && !isPosting && (
+      <button onClick={() => mutate({ content: input })}
+        >Post</button>
+    )}
+
+    {isPosting &&
+      <div className="flex justify-center items-center">
+        <LoadingSpinner size={20} />
+      </div>
+    }
   </div>
 }
 
@@ -84,22 +110,22 @@ const Feed = () => {
   if (!data) return <div>Something went wrong.</div>
   return (
     <div className="flex flex-col">
-            {data.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
   )
 }
 
 const Home: NextPage = () => {
 
-  const {isLoaded: userLoaded, isSignedIn} = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
   // Start fetching asap
   api.posts.getAll.useQuery();
 
   // Return empty div if BOTH are not loaded, since user tends to load faster
-  if (!userLoaded ) return <div></div>
+  if (!userLoaded) return <div></div>
 
 
 
