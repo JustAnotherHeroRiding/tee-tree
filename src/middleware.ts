f73d9c9c71d123590055d84e856ef75207ce9574
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-06-06 15:59:43 +0200

    changed middleware to allow trpc reqs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
+
 export default authMiddleware({
   publicRoutes: [
     "/",
-  '/api/trpc/posts.getAll'
-  ]
+
+    // let the api authentication be handled by trpc middleware
+    "/api(.*)",
+    "/trpc(.*)",
+    "/post/:id",
+    "/:slug",
+    
+  ],
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-/* Deprecated version */
-
-/* import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
-
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
-});
-
-// Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',}; */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
 import { PostView } from "~/components/postview";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -58,15 +59,15 @@ const CreatePostWizard = () => {
         if (e.key === "Enter") {
           e.preventDefault();
           if (input !== "") {
-            mutate({ content: input});
+            mutate({ content: input });
           }
         }
       }}
     />
     {input !== "" && !isPosting && (
       <button className="flex ml-auto px-4 mt-4 items-center rounded-3xl border border-slate-400 hover:bg-slate-700"
-       onClick={() => mutate({ content: input })}
-        >Post</button>
+        onClick={() => mutate({ content: input })}
+      >Post</button>
     )}
 
     {isPosting &&
@@ -105,22 +106,35 @@ const Home: NextPage = () => {
 
 
   return (
-      <PageLayout>
-                <div className="border-b border-slate-400 p-4 flex">
-            {!isSignedIn && <div className="flex justify-center">
-              <SignInButton />
-            </div>}
-            {isSignedIn &&
-              <div className="flex flex-col w-full">
-                <div className="flex ml-auto px-4 py-2 rounded-3xl border border-slate-400 hover:bg-slate-700 w-fit">
-                  <SignOutButton />
-                </div>
-                <CreatePostWizard />
+    <PageLayout>
+      <div className="border-b border-slate-400 p-4 flex">
+          
+        {!isSignedIn && <div className="flex flex-col w-full">
+          <SignInButton><div 
+              className="flex cursor-pointer ml-auto px-4 py-2 rounded-3xl border border-slate-400 hover:bg-slate-700 w-fit">
+                <h1>Sign In</h1>
+              </div></SignInButton>
+        </div>}
+        {isSignedIn &&
+          <div className="flex flex-col w-full">
+            <div className="flex flex-row pb-4 border-b border-slate-400 mb-4">
+              <Link href={"@justanotherhero"} 
+              className="flex cursor-pointer px-4 py-2 rounded-3xl border outline-none border-slate-400
+               hover:bg-slate-700 w-fit">Profile</Link>
+            <SignOutButton>                
+              <div 
+              className="flex cursor-pointer ml-auto px-4 py-2 rounded-3xl border border-slate-400 hover:bg-slate-700 w-fit">
+                <h1>Sign Out</h1>
               </div>
-            }
+            </SignOutButton>
             </div>
-          <Feed />
-          </PageLayout>
+
+            <CreatePostWizard />
+          </div>
+        }
+      </div>
+      <Feed />
+    </PageLayout>
   );
 };
 
