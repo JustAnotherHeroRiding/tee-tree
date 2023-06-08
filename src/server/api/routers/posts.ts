95200977aacc8ed6e209960c154cf28393cbb5f6
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-06-08 19:32:49 +0200

    allowed text and not just emoji

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -11,30 +11,30 @@ import type { Post } from "@prisma/client";
 
 const addUserDataToPosts = async (posts: Post[]) => {
   const users =
-      (await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-      ).map(filterUserForClient);
+    (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+    ).map(filterUserForClient);
 
 
-    return posts.map((post) => {
+  return posts.map((post) => {
 
-      const author = users.find((user) => user.id === post.authorId);
+    const author = users.find((user) => user.id === post.authorId);
 
-      if (!author || !author.username) throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Author for post not found"
-      });
-
-      return {
-        post,
-        author: {
-          ...author,
-          username: author.username,
-        },
-      };
+    if (!author || !author.username) throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Author for post not found"
     });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };
+  });
 }
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
@@ -46,43 +46,43 @@ const ratelimit = new Ratelimit({
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
    * instance with other applications and want to avoid key collisions. The default prefix is
    * "@upstash/ratelimit"
-   */ 
+   */
   prefix: "@upstash/ratelimit",
 });
 
 
 
 export const postsRouter = createTRPCRouter({
-  
+
   getById: publicProcedure
-  .input(z.object({ id: z.string() }))
-  .query(async ({ ctx, input }) => {
-    const post = await ctx.prisma.post.findUnique({
-      where: { id: input.id },
-    });
-
-    if (!post) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "Post not found",
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.findUnique({
+        where: { id: input.id },
       });
-    }
 
-    const postsWithUserData = await addUserDataToPosts([post]);
-    return postsWithUserData[0];
-  }),
+      if (!post) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Post not found",
+        });
+      }
+
+      const postsWithUserData = await addUserDataToPosts([post]);
+      return postsWithUserData[0];
+    }),
 
 
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
       orderBy: [
-        {createdAt: 'desc'}]
+        { createdAt: 'desc' }]
     });
 
 
     return addUserDataToPosts(posts);
-    
+
   }),
 
 
@@ -94,30 +94,31 @@ export const postsRouter = createTRPCRouter({
         authorId: input.userId,
       },
       take: 100,
-      orderBy: [{createdAt: 'desc'}],
+      orderBy: [{ createdAt: 'desc' }],
     }).then(addUserDataToPosts)
-    ), 
+    ),
 
   create: privateProcedure.input(
     z.object({
-    content: z.string().emoji("Only emojis are allowed.").min(1).max(280),
-  })
+      content: z.string().regex(/^(?:[\w\W]*?[a-zA-Z0-9][\w\W]*){1,280}$/).min(1).max(280),
+    })
   )
-  .mutation(async ({ ctx, input }) => {
-    const authorId = ctx.userId;
+
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
 
-    const { success } = await ratelimit.limit(authorId)
+      const { success } = await ratelimit.limit(authorId)
 
-    if (!success) throw new TRPCError({code : "TOO_MANY_REQUESTS"})
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" })
 
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId,
-        content: input.content,
-      }
-    });
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        }
+      });
 
-    return post;
-  }),
+      return post;
+    }),
 });
