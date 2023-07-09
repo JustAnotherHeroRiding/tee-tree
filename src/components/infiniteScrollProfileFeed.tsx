ee833bcb93747cdc7069878dc6ef18ca77136e73
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-07-09 18:14:26 +0200

    added retweed posts/link to user on profile feed

--- src/components/infiniteScrollProfileFeed.tsx
+++ src/components/infiniteScrollProfileFeed.tsx
@@ -3,8 +3,11 @@ import { api } from "~/utils/api";
 import { LoadingPage, LoadingSpinner } from "./loading";
 import { PostView } from "./postview";
 import type { PostWithAuthor } from "~/server/api/routers/posts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRetweet } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
-export const InfiniteScrollProfileFeed = (props: { userId: string }) => {
+export const InfiniteScrollProfileFeed = (props: { userId: string, username: string }) => {
   const [page, setPage] = useState(0);
 
   const {
@@ -75,11 +78,30 @@ export const InfiniteScrollProfileFeed = (props: { userId: string }) => {
 
           return isLastPost ? (
             <div key={fullPost.post.id} className="relative">
+              {fullPost.author.id !== props.userId && (
+                <div className="flex flex-row -mb-3">
+                  <FontAwesomeIcon icon={faRetweet} className="ml-8 h-6 w-6" />
+                  <h1 className="">Retweeted by</h1>
+                </div>
+              )}
+
               <PostView {...fullPost} />
-              <div ref={lastPostElementRef} className="infiniteScrollTriggerDiv"></div>
+              <div
+                ref={lastPostElementRef}
+                className="infiniteScrollTriggerDiv"
+              ></div>
             </div>
           ) : (
-            <PostView {...fullPost} key={fullPost.post.id} />
+            <>
+              {fullPost.author.id !== props.userId && (
+                  <Link className="flex flex-row -mb-3 hover:underline" href={`/@${props.username}`}>
+                  <FontAwesomeIcon icon={faRetweet} className="ml-12 my-auto h-4 w-4" />
+                  <h1 className="text-slate-300 ml-2">Retweeted by {props.username}</h1>
+                  </Link>
+              )}
+
+              <PostView {...fullPost} key={fullPost.post.id} />
+            </>
           );
         })
       )}
