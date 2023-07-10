d1fc854507048cfcdfa3fb6028872a061fce099d
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-07-10 11:36:24 +0200

    updated tailwind plus container queries

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,15 +18,15 @@ export const PageLayout = (props: PropsWithChildren) => {
               type="text"
               placeholder="Search"
               className="h-10 w-full rounded-full
-              border-2 border-Intone-300 px-4 py-2 text-black outline-none"
+              border-2 border-Intone-300 pl-6 pr-4 py-2 text-black outline-none"
             />
             <FontAwesomeIcon
               icon={faSearch}
-              className="absolute right-3 top-2 w-4 h-4 text-Intone-300"
+              className="absolute left-3 top-[14px] w-3 h-3 text-Intone-300"
             />
           </div>
-          <div className="flex flex-col items-center justify-center gap-6">
-            <div className="h-36 w-full items-center rounded-2xl border px-4 py-2 ">
+          <div className="flex flex-col items-center justify-center gap-6@">
+            <div className="h-36 w-full items-center rounded-2xl border px-4 py-2">
               <h1 className="font-bold">Trends for you</h1>
             </div>
             <div className="h-36 w-full items-center rounded-2xl border px-4 py-2 ">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,5 +63,7 @@ export default {
   },
   plugins: [
     tailwindScrollbar({ nocompatible: true }),
+    require('@tailwindcss/container-queries'),
+    
   ],
 } as Config;
