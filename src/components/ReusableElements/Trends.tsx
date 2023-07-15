de46a5c5169c60fec06c85e9b521c6bfab797d13
KRISTIJAN KOCEV <kristijankocev1234@gmail.com>
2023-07-15 20:15:43 +0200

    added limit to number of trends

--- src/components/ReusableElements/Trends.tsx
+++ src/components/ReusableElements/Trends.tsx
@@ -2,12 +2,10 @@ import { countHashtags } from "~/server/helpers/countHashtags";
 import { api } from "~/utils/api";
 import { LoadingSpinner } from "./loading";
 
-export const Trends = () => {
+export const Trends = ({ limit = 10 }) => {
   const { data: posts, isLoading } = api.posts.getAllLastWeek.useQuery();
-
-
+  
   if (!posts) {
-    
     return <div className="flex">
       <div className="mx-auto">
     <LoadingSpinner size={42} />
@@ -15,7 +13,11 @@ export const Trends = () => {
     </div>;
   }
 
+  // Sort and limit the hashtags
   const hashtagCounts = countHashtags(posts);
+  const sortedHashtagCounts = Object.entries(hashtagCounts).sort((a, b) => b[1] - a[1]);
+  
+  const topHashtags = sortedHashtagCounts.slice(0, limit); // limit is now a prop
 
   return (
     <div className="flex flex-col">
@@ -23,14 +25,14 @@ export const Trends = () => {
       <div className="mx-auto">
       <LoadingSpinner size={42} />
       </div>}
-      {Object.entries(hashtagCounts).map(([hashtag, count]) => (
-        <div className="flex flex-col px-4 pt-4
-         hover:bg-gray-900 cursor-pointer" key={hashtag}>
+      {topHashtags.map(([hashtag, count]) => (
+        <div className="flex flex-col 
+        px-4 pt-4 hover:bg-gray-900 cursor-pointer" key={hashtag}>
         <p className="font-bold">{hashtag}</p>
         <p className="text-twitter-200">{count} {`${count === 1 ? "post" : "posts"}`}</p>
         </div>
       ))}
-      <button className="text-Intone-300 hover:bg-gray-900 rounded-b-2xl 
+      <button className="text-Intone-300 hover:bg-gray-900 rounded-b-2xl
       text-start px-4 py-2">Show More</button>
     </div>
   );
