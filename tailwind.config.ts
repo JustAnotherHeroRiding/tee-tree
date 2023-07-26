00067d2c2691f29aa3bcbb3830c9b863208d4034
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-07-26 18:12:44 +0200

    added name next to logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
         'phone': {'max': '385px'},
         'showSidebar': {'max': '645px'},
         'trendsbreakpoint' : {'max': '1231px'},
+        'logoNameClip' : {'max': '728px'}
       },
       spacing: {
         '1/5': '20%',
