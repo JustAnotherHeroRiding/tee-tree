71724dfacc9baea3c80a14258cd14ea828ea429b
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-08-22 13:31:36 +0200

    mock tests wip

--- src/server/api/routers/__test/posts.infiniteScrollAllPosts.test.ts
+++ src/server/api/routers/__test/posts.infiniteScrollAllPosts.test.ts
@@ -1,6 +1,8 @@
 import { test, expect, afterAll } from "@jest/globals";
 import { appRouter } from "../../root";
 import { prisma } from "../../../db";
+import {mockDeep} from "jest-mock-extended"
+import { type PrismaClient } from "@prisma/client";
 
 afterAll(async () => {
   // Disconnect Prisma
@@ -19,5 +21,23 @@ test("Feed all posts test", async () => {
 
   expect(result.posts).toHaveLength(4);
 
-  console.log(result);
+});
+
+
+test("Initial Feed Mock test" , async () => {
+
+    const prismaMock = mockDeep<PrismaClient>();
+
+    const mockOutput : [string, number][] = [['#testing', 10]]
+
+    const caller = appRouter.createCaller({
+        session: null,
+        prisma: prismaMock,
+        userId: null,
+      });
+
+      const result = await caller.posts.getTrends({limit: 10});
+
+      expect(result).toHaveLength(mockOutput.length)
+      expect(result).toStrictEqual(mockOutput)
 });
