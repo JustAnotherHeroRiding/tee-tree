2c05219220acab4ebea290d5113e05fbe2a236e3
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-08-31 18:10:34 +0200

    let's see if it works now, the envs were not there

--- src/components/ReusableElements/CreatePostWizard.tsx
+++ src/components/ReusableElements/CreatePostWizard.tsx
@@ -20,7 +20,7 @@ import { Tooltip } from "react-tooltip";
 import { UserContext } from "../Context/UserContext";
 import { UserCard } from "./Users/UserMentionSuggestions";
 import EmojiSelector from "./EmojiPicker";
-import Pusher from 'pusher-js';
+import Pusher from "pusher-js";
 import { type ExtendedMessage } from "~/server/api/routers/messages";
 
 dayjs.extend(relativeTime);
@@ -346,20 +346,21 @@ export const CreatePostWizard: React.FC<CreatePostWizardProps> = ({
     },
   });
 
-
   useEffect(() => {
-    const pusher = new Pusher("APP_KEY", {
-      cluster: "APP_CLUSTER",
-    });
+    const pusher = new Pusher(
+      process.env.NEXT_PUBLIC_PUSHER_KEY as string,
+      {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
+      }
+    );
 
-    const channel = pusher.subscribe("my-channel");
+    const channel = pusher.subscribe("messagesUpdates");
 
-    channel.bind('new-message', (newMessage: ExtendedMessage) => {
+    channel.bind("new-message", (newMessage: ExtendedMessage) => {
       // Update your state or invalidate tRPC queries
-      console.log(newMessage)
+      console.log(newMessage);
       void ctx.messages.infiniteScrollMessagesWithUserId.invalidate();
     });
-    
 
     return () => {
       channel.unbind_all();
